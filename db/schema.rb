78a0b37f146383ced7846018a08979f418502a77
Kadeem Palacios <Main_Home@Kadeems-MacBook-Pro.local>
2016-12-06 18:57:22 -0800

    pins now include images

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206224333) do
+ActiveRecord::Schema.define(version: 20161207024208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_pins_on_user_id", using: :btree
   end
 

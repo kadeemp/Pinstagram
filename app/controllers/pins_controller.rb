9f2e8d76e22bd82819870e3d17ddc0bf9cf72c88
Kadeem Palacios <Main_Home@Kadeems-MacBook-Pro.local>
2016-12-06 17:09:25 -0800

    you can now  CRUD Pins. Some styling added

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,74 +1,58 @@
 class PinsController < ApplicationController
-  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+ before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
-  # GET /pins
-  # GET /pins.json
-  def index
-    @pins = Pin.all
-  end
 
-  # GET /pins/1
-  # GET /pins/1.json
-  def show
-  end
+ respond_to :html
 
-  # GET /pins/new
-  def new
-    @pin = Pin.new
-  end
+ def index
+   @pins = Pin.all
+ end
 
-  # GET /pins/1/edit
-  def edit
-  end
+ def show
+ end
 
-  # POST /pins
-  # POST /pins.json
-  def create
-    @pin = Pin.new(pin_params)
+ def new
+   @pin = current_user.pins.build
+ end
 
-    respond_to do |format|
-      if @pin.save
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
-        format.json { render :show, status: :created, location: @pin }
-      else
-        format.html { render :new }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+ def edit
+ end
 
-  # PATCH/PUT /pins/1
-  # PATCH/PUT /pins/1.json
-  def update
-    respond_to do |format|
-      if @pin.update(pin_params)
-        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
-        format.json { render :show, status: :ok, location: @pin }
-      else
-        format.html { render :edit }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+ def create
+   @pin = current_user.pins.build(pin_params)
+   if @pin.save
+     redirect_to @pin, notice: 'Pin was successfully created.'
+   else
+     render action: 'new'
+   end
+ end
 
-  # DELETE /pins/1
-  # DELETE /pins/1.json
-  def destroy
-    @pin.destroy
-    respond_to do |format|
-      format.html { redirect_to pins_url, notice: 'Pin was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+ def update
+  if @pin.update(pin_params)
+     redirect_to @pin, notice: 'Pin was successfully updated.'
+   else
+     render action: 'edit'
+   end
+ end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pin
-      @pin = Pin.find(params[:id])
-    end
+ def destroy
+   @pin.destroy
+   redirect_to pins_url
+ end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def pin_params
-      params.require(:pin).permit(:description)
-    end
+ private
+   def set_pin
+     @pin = Pin.find(params[:id])
+   end
+
+   def pin_params
+     params.require(:pin).permit(:description)
+   end
+
+
+ def correct_user
+     @pin = current_user.pins.find_by(id: params[:id])
+     redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+ end
+
 end

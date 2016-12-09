1e4969f53c044d89da0efc64dbba503c3ddbda4c
Kadeem Palacios <Main_Home@Kadeems-MacBook-Pro.local>
2016-12-09 11:42:08 -0800

    changed root to pins index, added homepage jumbotron to root

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'aws-sdk', '< 2.0'
 
 gem 'jquery-turbolinks'
 gem 'masonry-rails', '~> 0.2.4'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :pins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'pins#index'
   get 'home/index'
   get 'home/about'
   get 'users/edit'

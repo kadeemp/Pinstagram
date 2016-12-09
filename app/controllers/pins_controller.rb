7a4be5121b5083aac77f527a8e27100c6440d93a
Kadeem Palacios <Main_Home@Kadeems-MacBook-Pro.local>
2016-12-09 15:43:49 -0800

    nested comments added

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -11,6 +11,7 @@ class PinsController < ApplicationController
  end
 
  def show
+   @pin = Pin.find(params[:id])
  end
 
  def new
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :pins
+  resources :pins do
+    resources :comments
+  end
+
+  resources :comments do
+    resources :comments
+  end
   root 'pins#index'
   get 'home/index'
   get 'home/about'
